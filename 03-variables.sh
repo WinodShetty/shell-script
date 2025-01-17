fa89cb567450d1a4d9a529eb9d4b656bd8cf5b46
vinod <mechvnod@gmail.com>
2025-01-17 21:04:14 +0530

    special variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,6 +4,9 @@
 
 PERSON1=$1
 PERSON2=$2
+PERSON3=$3
+PERSON4=$4
+PERSON5=$5
 
 echo "$PERSON1: Hello $PERSON2"
 echo "$PERSON2: Hey $PERSON1"
@@ -11,5 +14,10 @@ echo "$PERSON1: How are you"
 echo "$PERSON2: I am Good, how are you"
 echo "$PERSON1: Great"
 
-#$1, $2, .... are special variables in shell
+
+echo $PERSON3
+echo $PERSON4
+echo $PERSON5
+
+#$1, $2, ,$3, $4, $5 .... are special variables in shell
 #we are using them as arguments
