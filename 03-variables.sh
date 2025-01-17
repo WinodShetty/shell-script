c7d3c0a57e1a8e82d3f423729814776524c90c5f
vinod <mechvnod@gmail.com>
2025-01-17 20:40:09 +0530

    variables with arguments

--- 03-variables.sh
+++ 03-variables.sh
@@ -10,3 +10,6 @@ echo "$PERSON2: Hey $PERSON1"
 echo "$PERSON1: How are you"
 echo "$PERSON2: I am Good, how are you"
 echo "$PERSON1: Great"
+
+#$1, $2, .... are special variables in shell
+#we are using them as arguments
