cf888b9c9293da3a49a290bb45599753d338b38b
vinod <mechvnod@gmail.com>
2025-01-17 20:50:47 +0530

    read

--- 04-variables.sh
+++ 04-variables.sh
@@ -6,5 +6,5 @@ echo "Enter your username:"
 read USERNAME
 echo "username entered is: $USERNAME"
 echo "Enter your password:"
-read -s PASSWORD
+read -s PASSWORD  # -s is to hide text
 echo "entered password is: $PASSWORD"
