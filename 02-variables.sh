1f2715cb5cf0cb0067843a28e2873a86d4f56fb1
vinod <mechvnod@gmail.com>
2025-01-17 20:28:54 +0530

    variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -5,7 +5,7 @@
 PERSON1=Dhoni
 PERSON2=Vinod
 
-echo "Hi $PERSON1 : Hi $PERSON2"
+echo "$PERSON1 : Hi $PERSON2"
 echo "$PERSON2 : Heloo $PERSON1"
 echo "$PERSON1 : How are you doing"
 echo "$PERSON2 : Doing well"
