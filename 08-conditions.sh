eefb28b0b8fa80b5303686c51412c8b07dfd80a8
vinod <mechvnod@gmail.com>
2025-01-17 21:59:21 +0530

    condtions

---
+++ 08-conditions.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+# -gt = > , -lt = < , -eq = = , -ge = >= , -le = <=
+NUMBER1=$1
+
+if [$NUMBER -gt 100]
+  then
+    echo "given number is greater than 100"
+else
+  echo "given number is less than 100"
+li
