a24526013620c7b62037a558704a66eeadede41c
vinod <mechvnod@gmail.com>
2025-03-11 11:38:23 +0530

    shell-script

--- install-mysql.sh
+++ install-mysql.sh
@@ -12,6 +12,14 @@ if [ $USERID -ne 0 ]
 fi
 
 dnf install mysql -y
+
+if [$? -ne 0 ]
+then 
+   echo "error: installing mysql is .....failure"
+   exit 1
+else
+   echo " error: installing mysql is .... success"
+
 dnf install git -y
    
     
