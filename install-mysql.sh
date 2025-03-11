e263b699a5a6a604de1b207af50b9b4a01856409
vinod <mechvnod@gmail.com>
2025-03-11 11:28:09 +0530

    shell-script

--- install-mysql.sh
+++ install-mysql.sh
@@ -2,22 +2,16 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ] 
- then
- 
-    echo "error:: you must have sudo acces to execute this script"
-    exit 1
-    
-    
-fi
- 
- dnf install mysql-server -y
-
-if [ $? -ne 0 ]
+if [ $USERID -ne 0 ]
  then 
-    echo "Installing MySQL ... is failure"
-    exit 1
- else 
-    echo "installing MySQL ... is success"
+   echo "error: you must have sudo access to excute this script"
+
 fi
+
+dnf install mysql -y
+   # echo $? exit status ni theluskovadaniki. 0 means success, 0 thappa yem vachina its failed
+   # exit status ante manam enter chesina last command, last command status
+    
+
+ 
 
